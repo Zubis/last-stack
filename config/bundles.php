b1b1b9d63374d60127c16a06d4e32eee7bd73075
Antoine Bourget <antoine.bourget.external@atos.net>
2024-02-21 17:48:49 +0100

    Fin du cour 13

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,5 @@ return [
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\UX\Autocomplete\AutocompleteBundle::class => ['all' => true],
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
+    BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
 ];
